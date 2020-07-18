041cc65a072aa3c1e293e65f3ac922c2370318e9
hewittwatkins <hewittwatkins@192.168.1.99>
2020-07-18 11:31:13 -0700

    Today I finished the array cheat sheet and I am working on the Robot Race.

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -4,24 +4,68 @@ public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
 		
+		String[] strArray = new String[] {"Hi","Hello","What's Up","Hey There","Hola"};
+		
+		
 		//2. print the third element in the array
+		
+		System.out.println(strArray[2]);
 		
 		//3. set the third element to a different value
 		
+		strArray[2] = "bob";
+		
 		//4. print the third element again
+		
+		System.out.println(strArray[2]);
 		
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
 		
+		for (int i = 0; i < strArray.length; i++) {
+			System.out.println(strArray[i]);
+		}
+		
 		
 		//6. make an array of 50 integers
 		
+		int[] intArray = new int[50];
+		for (int i = 0; i < 50; i++) {
+			intArray[i] = i;
+		}
+		
 		//7. use a for loop to make every value of the integer array a random number
+		
+		//did in #6
 		
 		//8. without printing the entire array, print only the smallest number in the array
 		
+		int smallNum = intArray[0];
+		for (int i = 1; i < intArray.length; i++) {
+			if (smallNum > intArray[i]) {
+				smallNum = intArray[i];
+			}
+	
+		}
+		System.out.println(smallNum);
+		
 		//9 print the entire array to see if step 8 was correct
 		
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+		
 		//10. print the largest number in the array.
+		
+		int bigNum = intArray[0];
+		for (int i = 1; i < intArray.length; i++) {
+			if (bigNum < intArray[i]) {
+				bigNum = intArray[i];
+			}
+	
+		}
+		System.out.println(bigNum);
+		
+		
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,16 +1,37 @@
 package arrays;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
 
+	public static void main(String[] args) {
+		
+	
 	//2. create an array of 5 robots.
 
+	Robot[] robots = new Robot[5];
+	
+		
 	//3. use a for loop to initialize the robots.
+	
+
+	for (int i = 0; i < 5; i++) {
+		robots[i] = new Robot();
+		robots[i].setX(50+(100*i));
+		robots[i].setY(450);
+		
+	}
 
 		//4. make each robot start at the bottom of the screen, side by side, facing up
+	
+	// in one above
+	
 
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
+	
+	
 
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
@@ -19,4 +40,7 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
+
+	
 }

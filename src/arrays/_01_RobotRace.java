15c088717806b4441471bbd8f79ce389fbb3157c
hewittwatkins <hewittwatkins@192.168.1.99>
2020-08-03 15:19:00 -0700

    Finished the Robot Race

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
@@ -31,15 +33,61 @@ public class _01_RobotRace {
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
 	
+	for (int robot = 0; robot < 5; robot++) {
+		Random num = new Random();
+		int integer = num.nextInt(51);
+		robots[robot].setY(integer + 50);
+	}
 	
 
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+	
+	int lowNum = 100;
+	int highestRobot = 0;
+	while (lowNum > 0) {
+		
+		for (int robot = 0; robot < 5; robot++) {
+			
+			Random num = new Random();
+			int integer = num.nextInt(51);
+			robots[robot].setY(integer);
+			
+			if ((integer) < lowNum) {
+				lowNum = integer;
+				highestRobot = robot;
+			}
+		}
+		
+	}
 
 	//7. declare that robot the winner and throw it a party!
+	
+	robots[highestRobot].sparkle();
 
 	//8. try different races with different amounts of robots.
+	
+	// did
 
 	//9. make the robots race around a circular track.
+	
+	for (int i = 0; i < 5; i++) {
+		robots[i].setX(300);
+		robots[i].setY(300);
+	}
+	
+	for (int i = 0; i < 5; i++) {
+		robots[i].move(50);
+		robots[i].turn(90);
+		robots[i].move(50);
+		robots[i].turn(90);
+		robots[i].move(50);
+		robots[i].turn(90);
+		robots[i].move(50);
+		robots[i].turn(90);
+		robots[i].move(50);
+	}
+	
+	
 }
 
 	
